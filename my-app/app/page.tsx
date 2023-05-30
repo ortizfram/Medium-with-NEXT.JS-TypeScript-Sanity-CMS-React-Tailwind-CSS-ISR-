56c50e26d6b20641ceeb07f10bdc915de0e519e9
melon <ortizfranco48@gmail.com>
2023-05-30 16:38:27 -0300

    header done

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto'>
       <Header />
 
-      <div>
-        <div>
+      <div className='flex border-y border-zinc-600 py-8 mt-3'>
+        <div className='px-10 space-y-5 text-zinc-600'>
           <h1 className='text-6xl max-w-xl font-serif'><span className='underline'>BuonaVibra</span> es un lugar para conectar con tu yo elevado</h1>
           <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est commodi, ad harum esse incidunt exercitationem pariatur iure? Nemo, labore sed.</h2>
         </div>
--- my-app/components/Header.tsx
+++ my-app/components/Header.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 function Header() {
     return (
-        <header className='flex justify-between p-5 mx-auto'>
+        <header className='flex justify-between p-5 mx-auto my-[-40px]'>
             <div className='flex items-center space-x-5'>
                 <Link href="/">
-                    <img className='mt-0 h-[18vh] object-contain cursor-pointer' src="/buonavibra-logo.png" alt="Buona-vibra-club-logo" />
+                    <img className='mt-5 mb-2 h-[10vh] object-contain cursor-pointer' src="/buonavibra-logo.png" alt="Buona-vibra-club-logo" />
                 </Link>
                 <div className='hidden lg:inline-flex items-center space-x-5'>
                     <h3>About</h3>
@@ -17,7 +17,7 @@ function Header() {
                 </div>
             </div>
 
-            <div className='flex items-center space-x-5 text-green-600'>
+            <div className='flex items-center space-x-5 text-green-600 font-semibold'>
                 <h3>Sign in</h3>
                 <h3 className='border px-4 py-1 rounded-full border-green-600'>Get started</h3>
             </div>
