2cf43b65b2a08470fa6b1efd47fd2a9d13ee82f1
ortizfram <ortizfranco48@gmail.com>
2023-05-30 13:57:30 -0300

    hero div1

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -6,6 +6,17 @@ export default function Home() {
   return (
     <div>
       <Header />
+
+      <div>
+        <div>
+          <h1 className='text-6xl max-w-xl font-serif'><span className='underline'>BuonaVibra</span> es un lugar para conectar con tu yo elevado</h1>
+          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est commodi, ad harum esse incidunt exercitationem pariatur iure? Nemo, labore sed.</h2>
+        </div>
+
+        <div>
+
+        </div>
+      </div>
     </div>
   )
 }
