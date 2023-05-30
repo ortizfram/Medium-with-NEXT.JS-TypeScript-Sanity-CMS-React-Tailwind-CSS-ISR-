f447b8095ce8f316f4237a5e89da40245772f031
ortizfram <ortizfranco48@gmail.com>
2023-05-30 13:50:05 -0300

    header.tsx

---
+++ my-app/components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Header() {
+    return (
+        <header className='flex justify-between p-5 mx-auto'>
+            <div className='flex items-center space-x-5'>
+                <Link href="/">
+                    <img className='mt-0 h-[18vh] object-contain cursor-pointer' src="/buonavibra-logo.png" alt="Buona-vibra-club-logo" />
+                </Link>
+                <div className='hidden lg:inline-flex items-center space-x-5'>
+                    <h3>About</h3>
+                    <h3>Contact</h3>
+                    <h3>Class</h3>
+                    <h3>Workshop</h3>
+                    <h3 className='px-4 py-1 rounded-full text-white bg-green-600'>follow</h3>
+                </div>
+            </div>
+
+            <div className='flex items-center space-x-5 text-green-600'>
+                <h3>Sign in</h3>
+                <h3 className='border px-4 py-1 rounded-full border-green-600'>Get started</h3>
+            </div>
+        </header>
+    )
+}
+
+export default Header
